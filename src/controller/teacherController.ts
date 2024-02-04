6a5767ffbb50897abd4a294c69f77a72693c7cd6
amaz11 <k.m.shaonred@gmail.com>
2024-02-04 23:50:26 +0600

    file change

--- src/controller/teacherController.ts
+++ src/controller/teacherController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express'
-// const readXlsxFile = require('read-excel-file/node')
 import * as xlsx from 'xlsx'
 
 const createTeacher = async (req: Request, res: Response) => {
     let exclePath = req?.file?.path
-    // const row = await readXlsxFile(exclePath)
     const workbook = xlsx.readFile(`${exclePath}`);  // Step 2
     let workbook_sheet = workbook.SheetNames;                // Step 3
     let workbook_response = xlsx.utils.sheet_to_json(        // Step 4
