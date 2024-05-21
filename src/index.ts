621016423594cd85cb0aabc26ae0c0c21478de41
amaz11 <k.m.shaonred@gmail.com>
2024-05-21 20:11:35 +0600

    package unistall

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import cors from 'cors'
 import compression from "compression";
 import cookieParser from "cookie-parser";
